814fd621e6348d7826b42293596881758541665b
Christian Stein <sormuras@gmail.com>
2018-02-04 02:14:03 +0100

    Use Java 10 in Bach build.jsh script and introduce Build.java

--- build.jsh
+++ build.jsh
@@ -1,31 +1,9 @@
 //usr/bin/env jshell --show-version --execution local "$0" "$@"; exit $?
 
 /*
- * Define global properties.
- */
-String name = "com.github.forax.beautifullogger"
-
-String junitPlatformVersion = "1.1.0-M2" // bundles the corresponding Jupiter "5.x.y[-z]"
-
-Path sources = Paths.get("src/main/java")
-Path tests = Paths.get("src/test/java")
-
-Path classes = Paths.get("bin/bach/classes")
-Path javadoc = Paths.get("bin/bach/javadoc")
-
-Path beautifulJar = Paths.get("bin/bach/beautifullogger.jar")
-Path beautifulSources = Paths.get("bin/bach/beautifullogger-sources.jar")
-Path beautifulJavadoc = Paths.get("bin/bach/beautifullogger-javadoc.jar")
-
-/*
- * Switch Bach to verbose mode. That'll print the commands before execution.
- */
-System.setProperty("bach.verbose", "true")
-
-/*
  * Download "Bach.java" and "Bach.jsh" from github to local binary directory.
  */
-Path target = Files.createDirectories(Paths.get("bin/bach"))
+Path target = Files.createDirectories(Paths.get("bin/bach/download"))
 URL context = new URL("https://raw.githubusercontent.com/sormuras/bach/1.0.0/src/bach/")
 for (Path script : Set.of(target.resolve("Bach.java"), target.resolve("Bach.jsh"))) {
     if (Files.exists(script)) continue; // comment to force download files
@@ -37,44 +15,25 @@ for (Path script : Set.of(target.resolve("Bach.java"), target.resolve("Bach.jsh"
 /*
  * Source "Bach.java" and "Bach.jsh" into this jshell session.
  */
-/open bin/bach/Bach.java
-/open bin/bach/Bach.jsh
+/open bin/bach/download/Bach.java
+/open bin/bach/download/Bach.jsh
 
 java("--version")
 
 /*
- * Compile.
- *
- * Compile using "--module-source-path" and "--module" combo fails:
- * <pre>
- *   javac("-d", classes.resolve("main"), "--module-source-path", sources, "--module", name)
- *   error: module com.github.forax.beautifullogger not found in module source path
- * </pre>
- * So, we have to simulate "$(find src -name '*.java')" using a self-expanding visitor.
+ * Source "Build.java"
  */
-javac("-d", classes.resolve("main"), Bach.Command.visit(command -> command.addAllJavaFiles(sources)))
-
+/open src/bach/Build.java
 
 /*
- * Javadoc.
+ * Build the project.
  */
-javadoc("-html5", "-quiet", "-Xdoclint:all,-missing", "-linksource", "-link", "https://docs.oracle.com/javase/9/docs/api", "-d", javadoc, Bach.Command.visit(command -> command.addAllJavaFiles(sources)))
+var error = 0
+try {
+  Build.main();
+} catch (Throwable throwable) {
+  throwable.printStackTrace();
+  error = 1;
+}
 
-/*
- * Package.
- */
-jar("--create", "--file", beautifulJar, "-C", classes.resolve("main"), ".")
-jar("--create", "--file", beautifulSources, "-C", sources, ".")
-jar("--create", "--file", beautifulJavadoc, "-C", javadoc, ".")
-
-/*
- * Load and use JUnit Platform Console Standalone distribution for compiling and running tests.
- */
-Path junit = new Bach.Basics.Resolvable("org.junit.platform", "junit-platform-console-standalone", junitPlatformVersion).resolve(Paths.get("bin/bach/tools/junit"), Bach.Basics.Resolvable.REPOSITORIES)
-
-String testClassPath = String.join(File.pathSeparator, beautifulJar.toString(), junit.toString())
-javac("-d", classes.resolve("test"), "--class-path", testClassPath, Bach.Command.visit(command -> command.addAllJavaFiles(tests)))
-
-java("-ea", "-jar", junit, "--class-path", classes.resolve("test"), "--class-path", beautifulJar, "--scan-classpath");
-
-/exit
+/exit error
---
+++ src/bach/Build.java
@@ -0,0 +1,127 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+interface Build {
+
+	Path SOURCE_MAIN = Paths.get("src", "main", "java");
+	Path SOURCE_TEST = Paths.get("src", "test", "java");
+
+	Path MODULES = Paths.get("bin", "bach", "modules");
+	Path TARGET = Paths.get("bin", "bach", "target");
+	Path TARGET_MAIN = TARGET.resolve("classes/main");
+	Path TARGET_TEST = TARGET.resolve("classes/test");
+	Path JAVADOC = TARGET.resolve("javadoc");
+	Path ARTIFACTS = TARGET.resolve("artifacts");
+
+
+	static void main(String... args) throws Exception {
+		System.out.printf("%n[build]%n%n");
+		resolveRequiredModules();
+
+		Bach.log.level = Bach.Log.Level.VERBOSE;
+		compileMain();
+		javadoc();
+		jar();
+		jdeps();
+		compileTest();
+		test();
+	}
+
+	static void resolveRequiredModules() {
+		// "JUnit 5"
+		var jupiterVersion = "5.1.0-M2";
+		var platformVersion = "1.1.0-M2";
+		resolve("org.junit.jupiter", "junit-jupiter-api", jupiterVersion);
+		resolve("org.junit.jupiter", "junit-jupiter-engine", jupiterVersion);
+		resolve("org.junit.jupiter", "junit-jupiter-params", jupiterVersion);
+		resolve("org.junit.platform", "junit-platform-console", platformVersion);
+		resolve("org.junit.platform", "junit-platform-commons", platformVersion);
+		resolve("org.junit.platform", "junit-platform-engine", platformVersion);
+		resolve("org.junit.platform", "junit-platform-launcher", platformVersion);
+		// 3rd-party modules
+		resolve("org.opentest4j", "opentest4j", "1.0.0");
+		resolve("org.apiguardian", "apiguardian-api", "1.0.0");
+	}
+
+	static Path resolve(String group, String artifact, String version) {
+		return new Bach.Basics.Resolvable(group, artifact, version).resolve(MODULES, Bach.Basics.Resolvable.REPOSITORIES);
+	}
+
+	static void compileMain() {
+		System.out.printf("%n[main][compile]%n%n");
+		var javac = new Bach.JdkTool.Javac();
+		javac.generateAllDebuggingInformation = true;
+		javac.destination = TARGET_MAIN;
+		javac.moduleSourcePath = List.of(SOURCE_MAIN);
+		javac.modulePath = List.of(MODULES);
+		javac.run();
+	}
+
+	static void compileTest() {
+		System.out.printf("%n[test][compile]%n%n");
+		var javac = new Bach.JdkTool.Javac();
+		javac.destination = TARGET_TEST;
+		javac.moduleSourcePath = List.of(SOURCE_TEST);
+		javac.modulePath = List.of(MODULES);
+		javac.patchModule = Bach.Basics.getPatchMap(List.of(SOURCE_TEST), List.of(SOURCE_MAIN));
+		javac.run();
+	}
+
+	static void test() {
+		System.out.printf("%n[test]%n%n");
+		var java = new Bach.JdkTool.Java();
+		java.modulePath = List.of(TARGET_TEST, MODULES);
+		java.addModules = List.of("ALL-MODULE-PATH");
+		java.module = "org.junit.platform.console";
+		java.args = List.of("--scan-modules");
+		java.run();
+	}
+
+	static void javadoc() throws Exception {
+		System.out.printf("%n[javadoc]%n%n");
+		Files.createDirectories(JAVADOC);
+		Bach.run(
+				"javadoc",
+				"-html5",
+				"-quiet",
+				"-Xdoclint:all,-missing",
+				"-package",
+				"-linksource",
+				"-link",
+				"https://docs.oracle.com/javase/9/docs/api",
+				"-d",
+				JAVADOC,
+				"-sourcepath",
+				SOURCE_MAIN.resolve("com.github.forax.beautifullogger"),
+				//"--module-source-path",
+				//SOURCE_MAIN,
+				"-subpackages",
+				"com");
+	}
+
+	static void jar() throws Exception {
+		System.out.printf("%n[jar]%n%n");
+		Files.createDirectories(ARTIFACTS);
+		jar("beautiful-logger.jar", TARGET_MAIN);
+		jar("beautiful-logger-sources.jar", SOURCE_MAIN);
+		jar("beautiful-logger-javadoc.jar", JAVADOC);
+	}
+
+	private static void jar(String artifact, Path path) {
+		var jar = new Bach.JdkTool.Jar();
+		jar.file = ARTIFACTS.resolve(artifact);
+		jar.path = path;
+		jar.run();
+	}
+
+	static void jdeps() {
+		System.out.printf("%n[jdeps]%n%n");
+
+		var jdeps = new Bach.JdkTool.Jdeps();
+		jdeps.summary = true;
+		jdeps.recursive = true;
+		jdeps.toCommand().add(ARTIFACTS.resolve("beautiful-logger.jar")).run();
+	}
+}
