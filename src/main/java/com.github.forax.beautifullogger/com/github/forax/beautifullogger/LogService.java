40c7ca771dc4478dc3c19eded38cd60078feac17
forax <forax@univ-mlv.fr>
2018-02-09 18:38:57 +0100

    add javadoc

--- src/main/java/com.github.forax.beautifullogger/com/github/forax/beautifullogger/LogService.java
+++ src/main/java/com.github.forax.beautifullogger/com/github/forax/beautifullogger/LogService.java
@@ -6,6 +6,28 @@ import com.github.forax.beautifullogger.Logger.Level;
 
 /**
  * An interface that you can extend if you want to create your own log service.
+ * 
+ * The method {@link #getService(Lookup, Class, Class)} provide an object of a
+ * class that implement the service interface if either you create an interface
+ * that extends {@link Logger} or {@link LogService} or you create an interface
+ * with abstract methods with the same name and signatures as the ones in
+ * {@link Logger} or {@link LogService}.
+ * 
+ * <pre>
+ * interface MethodLoggerService extends Logger {
+ *   default void logEnter() {
+ *     info("enter", null);
+ *   }
+ * 
+ *   default void logExit() {
+ *     info("exit", null);
+ *   }
+ * 
+ *   static MethodLoggerService getService() {
+ *     return LogService.getService(MethodHandles.lookup(), MethodLoggerService.class);
+ *   }
+ * }
+ * </pre>
  */
 public interface LogService {
   /**
@@ -16,10 +38,10 @@ public interface LogService {
    * @param level the logging level that will be checked against the {@link LoggerConfig#level() logger configuration level}.
    * @param context the exception to log or null. 
    * @param messageProvider a message provider, a String or a lambda.
-   * @param arg0 the first argument or {@link LogServiceSPI#NONE}.
-   * @param arg1 the second argument or {@link LogServiceSPI#NONE}.
-   * @param arg2 the third argument or {@link LogServiceSPI#NONE}.
-   * @param arg3 the fourth argument or {@link LogServiceSPI#NONE}.
+   * @param arg0 the first argument or {@link #NONE}.
+   * @param arg1 the second argument or {@link #NONE}.
+   * @param arg2 the third argument or {@link #NONE}.
+   * @param arg3 the fourth argument or {@link #NONE}.
    */
   void log(Level level, Throwable context, Object messageProvider, Object arg0, Object arg1, Object arg2, Object arg3);
 
@@ -35,6 +57,7 @@ public interface LogService {
    * Calling this method is equivalent a call to
    * {@link #getService(Lookup, Class, Class) getService(lookup, serviceInterface, serviceInterface)}.
    * 
+   * @param <T> the type of service.
    * @param lookup a lookup object that have access to the service interface.
    * @param serviceInterface the service interface to implement.
    * @return an object of a class that implement that service.
@@ -54,6 +77,7 @@ public interface LogService {
    * as one abstract method of the interface {@link Logger} or the interface {@link LogService},
    * the default method will be overridden.
    * 
+   * @param <T> the type of service.
    * @param lookup a lookup object that have access to the service interface.
    * @param serviceInterface the service interface to implement.
    * @param configClass class that will use to find the configuration of the log service. 
--- src/main/java/com.github.forax.beautifullogger/com/github/forax/beautifullogger/LoggerConfig.java
+++ src/main/java/com.github.forax.beautifullogger/com/github/forax/beautifullogger/LoggerConfig.java
@@ -7,22 +7,94 @@ import static com.github.forax.beautifullogger.LoggerImpl.LoggerConfigKind.PACKA
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Consumer;
 
 import com.github.forax.beautifullogger.Logger.Level;
 
+/**
+ * The configuration of a {@link Logger}.
+ * 
+ * The configuration can be set on a {@link LoggerConfig#fromModule(Module) module},
+ * a {@link LoggerConfig#fromPackage(Package) package} or
+ * a {@link LoggerConfig#fromClass(Class) class}.
+ * When a logger read a configuration property, it first tries to read the configuration property
+ * of its configuration class, if the property is not defined, it tries to read the configuration property of the package
+ * and if it is not defined, it tries to read the configuration property of the module, if it is not defined,
+ * it uses the default values below.
+ * 
+ * <table>
+ *  <tr><td>enable</td><td>true</td></tr>
+ *  <tr><td>level</td><td>{@link Level#INFO}</td></tr>
+ *  <tr><td>printFactory</td><td>@link {@link PrintFactory#systemLogger() System Logger}</td></tr>
+ *  <caption>default value of the configuration properties</caption>
+ * </table>
+ * 
+ * <pre>
+ * class Example {
+ *   private static final Logger LOGGER = Logger.getLogger(Example.class);
+ *   
+ *   public static void main(String[] args) {
+ *     LogerConfig config = LoggerConfig.fromClass(Example.class);
+ *     System.out.println("logger enabled" + config.enable());
+ *     ...
+ * </pre>
+ * 
+ * Because changing a configuration property may cause the Java Virtual Machine to de-optimize assembly codes,
+ * the change to the properties of a LoggerConfig has to be in bulk using the method{@link LoggerConfig#update(Consumer)}.
+ * 
+ * By example to change the level of a configuration
+ * <pre>
+ *    config.update(opt -&gt; opt.level(Level.TRACE)); 
+ * </pre>
+ */
 public interface LoggerConfig {
+  /**
+   * The interface to intercept the logging events after that logging level
+   * have been been verified and the message have been computed.
+   * 
+   * @see PrintFactory#printer(Printer)
+   */
   @FunctionalInterface
   interface Printer {
+    /**
+     * Emit the logging event.
+     * 
+     * @param message the event message
+     * @param level the event level
+     * @param context the event exception or null.
+     */
     void print(String message, Level level, Throwable context);
   }
   
+  /**
+   * The interface that provides a method handle that can emit
+   * a logging event for a configuration class.
+   * 
+   * @see LoggerConfig#printFactory()
+   * @see ConfigOption#printFactory(PrintFactory)
+   */
   @FunctionalInterface
   interface PrintFactory {
+    /**
+     * Returns a method handle that can emit a logging event for a configuration class.
+     * This method is not called for each event but more or less each time
+     * the runtime detects that the configuration has changed.
+     * 
+     * @param configClass the configuration class.
+     * @return a method handle that can emit a logging event.
+     */
     MethodHandle getPrintMethodHandle(Class<?> configClass);
     
+    /**
+     * Create a PrintFactory from a printer
+     * @param printer a printer.
+     * @return a new PrintFactory that delegate the logging to the printer.
+     * @throws NullPointerException if the printer is null.
+     */
     static PrintFactory printer(Printer printer) {
+      Objects.requireNonNull(printer);
       MethodHandle mh;
       try {
         mh = MethodHandles.lookup().findVirtual(Printer.class, "print",
@@ -34,6 +106,10 @@ public interface LoggerConfig {
       return __ -> target;
     }
     
+    /**
+     * Create a PrintFactory from the {@link System.Logger system logger}.
+     * @return a new PrintFactory that delegate the logging to the {@link System.Logger system logger}.
+     */
     static PrintFactory systemLogger() {
       MethodHandle mh, filter;
       try {
@@ -78,35 +154,111 @@ public interface LoggerConfig {
   }
   
   
-  
+  /**
+   *  The option that can be mutated by the method {@link LoggerConfig#update(Consumer)}
+   *  of a {@link LoggerConfig}.
+   */
   interface ConfigOption {
+    /**
+     * Update the configuration to enable/disable the loggers.
+     * @param enable true to enable the logging.
+     * @return this configuration option
+     */
     ConfigOption enable(boolean enable);
+    /**
+     * Update the configuration level.
+     * @param level the accepted logging level.
+     * @return this configuration option
+     * @throws NullPointerException if the level is null
+     */
     ConfigOption level(Level level);
+    /**
+     * Update the configuration property enable.
+     * @param factory the print factory to use
+     * @return this configuration option
+     * @throws NullPointerException if the factory is null
+     */
     ConfigOption printFactory(PrintFactory factory);
   }
   
+  /**
+   * Returns if the logging is enable, disable or not set.
+   * @return if the logging is enable, disable or not set.
+   */
   Optional<Boolean> enable();
+  /**
+   * Returns the logging level if set.
+   * @return the logging level if set.
+   */
   Optional<Level> level();
+  /**
+   * Returns the logging print factory if set.
+   * @return the logging print factory if set.
+   */
   Optional<PrintFactory> printFactory();
   
+  /**
+   * Update the configuration by updating the value and then commit the changes. 
+   * @param configUpdater a lambda that can update the configuration.
+   * @return this logger configuration.
+   */
   LoggerConfig update(Consumer<? super ConfigOption> configUpdater);
   
+  /**
+   * Returns the configuration associated with the configuration class.
+   * @param configClass the configuration class.
+   * @return the  configuration associated with the configuration class.
+   * @throws NullPointerException if the configuration class is null.
+   */
   static LoggerConfig fromClass(Class<?> configClass) {
-    return fromClass(configClass.getName());
+    return fromClass(configClass.getName());  // implicit null check
   }
+  /**
+   * Returns the configuration associated with the configuration class.
+   * @param className the configuration class name.
+   * @return the  configuration associated with the configuration class.
+   * @throws NullPointerException if the configuration class name is null.
+   */
   static LoggerConfig fromClass(String className) {
+    Objects.requireNonNull(className);
     return LoggerImpl.configFrom(CLASS, className);
   }
+  /**
+   * Returns the configuration associated with the configuration package.
+   * @param packaze the configuration package.
+   * @return the  configuration associated with the configuration package.
+   * @throws NullPointerException if the configuration package is null.
+   */
   static LoggerConfig fromPackage(Package packaze) {
-    return fromPackage(packaze.getName());
+    return fromPackage(packaze.getName());  // implicit null check
   }
+  /**
+   * Returns the configuration associated with the configuration package.
+   * @param packageName the configuration package name.
+   * @return the  configuration associated with the configuration package.
+   * @throws NullPointerException if the configuration package name is null.
+   */
   static LoggerConfig fromPackage(String packageName) {
+    Objects.requireNonNull(packageName);
     return LoggerImpl.configFrom(PACKAGE, packageName);
   }
+  /**
+  * Returns the configuration associated with the configuration module.
+  * @param module the configuration module.
+  * @return the  configuration associated with the configuration module.
+  * @throws NullPointerException if the configuration module is null.
+  */
   static LoggerConfig fromModule(Module module) {
-    return fromModule(module.getName());
+    return fromModule(module.getName());  // implicit null check
   }
+  /**
+   * Returns the configuration associated with the configuration module.
+   * @param moduleName the configuration module name.
+   * @return the  configuration associated with the configuration module.
+   * @throws NullPointerException if the configuration module name is null.
+   */
   static LoggerConfig fromModule(String moduleName) {
+    Objects.requireNonNull(moduleName);
     return LoggerImpl.configFrom(MODULE, moduleName);
   }
 }
