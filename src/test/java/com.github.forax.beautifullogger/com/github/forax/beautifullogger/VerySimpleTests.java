c54c13a114b1ec4bcfa2d7710158a2935cca755e
forax <forax@univ-mlv.fr>
2018-02-09 12:15:03 +0100

    Align the test to be close to the example used in the README

--- src/test/java/com.github.forax.beautifullogger/com/github/forax/beautifullogger/VerySimpleTests.java
+++ src/test/java/com.github.forax.beautifullogger/com/github/forax/beautifullogger/VerySimpleTests.java
@@ -6,10 +6,17 @@ import com.github.forax.beautifullogger.Logger;
 
 @SuppressWarnings("static-method")
 class VerySimpleTests {
-  private static final Logger LOGGER = Logger.getLogger(VerySimpleTests.class);
+  private static final Logger LOGGER = Logger.getLogger();
   
   @Test
   void justAVerySimpleTest() {
-    LOGGER.error(() -> "message");
+    for(int i = 0; i < 10; i++) {
+      LOGGER.error((int value) -> "message " + value, i);
+      
+      if (i == 1) {
+        LoggerConfig.fromClass(VerySimpleTests.class)
+          .update(opt -> opt.enable(false));
+      }
+    }
   }
 }
