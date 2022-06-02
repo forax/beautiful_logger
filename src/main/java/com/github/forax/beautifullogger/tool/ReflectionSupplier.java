853f9d260daad5784de8231800fac874921a86d8
forax <forax@univ-mlv.fr>
2022-06-02 11:50:51 +0200

    fix: empty class does not seems to work with java 9 javadoc

--- src/main/java/com/github/forax/beautifullogger/tool/ReflectionSupplier.java
+++ src/main/java/com/github/forax/beautifullogger/tool/ReflectionSupplier.java
@@ -2,10 +2,10 @@ package com.github.forax.beautifullogger.tool;
 
 import java.util.function.Supplier;
 
-/*
 public class ReflectionSupplier implements Supplier<Class<?>> {
   @Override
   public Class<?> get() {
-    return sun.reflect.Reflection.getCallerClass(5);
+    //return sun.reflect.Reflection.getCallerClass(5);
+    throw new UnsupportedOperationException();
   }
-}*/
+}
