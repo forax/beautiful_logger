db5347bd24d1dbf79a5cc9efd3abba5673fa4d1f
forax <forax@univ-mlv.fr>
2022-06-02 11:52:47 +0200

    fix: empty class does not seems to work with java 9 javadoc

--- src/main/java/com/github/forax/beautifullogger/tool/ReflectionSupplierEncoder.java
+++ src/main/java/com/github/forax/beautifullogger/tool/ReflectionSupplierEncoder.java
@@ -3,13 +3,13 @@ package com.github.forax.beautifullogger.tool;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Base64;
-/*
+
 public class ReflectionSupplierEncoder {
   public static void main(String[] args) throws IOException {
-    InputStream input = ReflectionSupplierEncoder.class.getResourceAsStream("ReflectionSupplier.class");
-    byte[] code = input.readAllBytes();
-    byte[] rewritedCode = Rewriter.rewrite(code);
-    System.out.println(Base64.getEncoder().encodeToString(rewritedCode));
+    //InputStream input = ReflectionSupplierEncoder.class.getResourceAsStream("ReflectionSupplier.class");
+    //byte[] code = input.readAllBytes();
+    //byte[] rewritedCode = Rewriter.rewrite(code);
+    //System.out.println(Base64.getEncoder().encodeToString(rewritedCode));
   }
 }
-*/
+
